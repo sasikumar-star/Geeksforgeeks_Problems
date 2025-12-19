718d2fa4ac6e53c190ea3f3f0a74b259de9ac0c6
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2025-12-19 11:49:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Conductor/bus-conductor.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int findMoves(vector<int>& chairs, vector<int>& passengers) {
+        // code here
+        sort(chairs.begin(),chairs.end());
+        sort(passengers.begin(),passengers.end());
+        int sum=0;
+        for(int i=0;i<chairs.size();i++){
+                 sum+=abs(chairs[i]-passengers[i]);
+                }
+        return sum;
+    }
+};
+
